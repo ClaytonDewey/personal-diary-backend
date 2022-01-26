a72b6ca6a39d19ca105c231520709e7cd1e6657f
Clay <clay@dryadmedia.com>
2022-01-25 18:41:59 -0600

    Import routes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-// import auth from "./routes/auth";
-// import notes from "./routs/auth";
+import auth from "./routes/auth";
+import notes from "./routs/auth";
 
 mongoose.connect("mongodb://localhost/personaldiary_db", {
   useNewUrlParser: true,
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
   res.send("It works!");
 });
 
-// auth(app);
-// notes(app);
+notes(app);
+auth(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
